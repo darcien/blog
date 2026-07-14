b37ee0332759b67995baba3fdb996aab4dc5c496
Yosua Ian Sebastian <git@darcien.me>
2026-07-14 23:55:15 +1000

    revert prefetch script
    it's like 50% transferred size of the homepage with zstd (it's 1.16kB), but still, no thanks
    
    This reverts commit d7915b70d240e1b4cdc7b8d41844d89f3aef8380.

--- astro.config.mjs
+++ astro.config.mjs
@@ -68,7 +68,6 @@ export default defineConfig({
   site: "https://darcien.me/",
   trailingSlash: "never",
   redirects: { "/coffee": "/garden/coffee" },
-  prefetch: { prefetchAll: true, defaultStrategy: "hover" },
   integrations: [mdx(), sitemap()],
   markdown: {
     processor: satteri({
